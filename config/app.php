c156cc02ba2513adbdd3568afa429e51e7175a25
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2020-01-31 01:04:48 -0300

    dsfas

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Clarification\MailDrivers\Sparkpost\SparkpostServiceProvider::class,
 
         /*
          * Package Service Providers...
