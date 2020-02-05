e4048985111ceceab05aa5fba16965c0edb8770c
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2020-02-05 13:42:06 -0300

    dsfasfdas

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -134,8 +134,8 @@ class ToolsController extends Controller
         $this->clearTeste1Folder();
         $slugify = new \Cocur\Slugify\Slugify();
         $stateSlug = 'sao-paulo';
-        $citySlug = 'ribeirao-preto';
-        $url = 'https://www.travesticomlocal.com.br/ribeirao-preto/';
+        $citySlug = 'bauru';
+        $url = 'https://www.travesticomlocal.com.br/bauru';
 
         $stateFind = $stateModel->where('slug', $stateSlug)->first();
         $cityFind = $cityModel->where('slug', $citySlug)->where('state_id', $stateFind->id)->first();
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        DB::update("SET @@auto_increment_increment=1");
     }
 }
