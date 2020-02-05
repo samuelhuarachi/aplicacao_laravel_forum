640cdf7b34b6ace16d03a6d7d4a09b7d2abf6f90
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2020-02-05 02:00:51 -0300

    sdfsaf

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,13 @@ Route::get('/', function () {
 // Route::get('tools/extract-images', [
 //     'uses' => 'ToolsController@extractImagesFromWebSite']);
 
-Route::get('tools/full-scan', [
-    'uses' => 'ToolsController@fullScan']);
+// Route::get('tools/full-scan', [
+//     'uses' => 'ToolsController@fullScan']);
     
 
 
-Route::get('/sitemap/generate', [
-    'uses' => 'SitemapController@generate']);
+// Route::get('/sitemap/generate', [
+//     'uses' => 'SitemapController@generate']);
 
 Route::group(['prefix' => 'forum/travesti'],
 function() {
