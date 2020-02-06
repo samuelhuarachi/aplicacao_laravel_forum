a2e90a761e78c6d0f0980e883f3770bf21711452
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2020-02-06 14:47:31 -0300

    script-full-scan

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -134,8 +134,8 @@ class ToolsController extends Controller
         $this->clearTeste1Folder();
         $slugify = new \Cocur\Slugify\Slugify();
         $stateSlug = 'santa-catarina';
-        $citySlug = 'florianopolis';
-        $url = 'https://www.travesticomlocal.com.br/florianopolis/';
+        $citySlug = 'joinville';
+        $url = 'https://www.travesticomlocal.com.br/joinville/';
 
         $stateFind = $stateModel->where('slug', $stateSlug)->first();
         $cityFind = $cityModel->where('slug', $citySlug)->where('state_id', $stateFind->id)->first();
