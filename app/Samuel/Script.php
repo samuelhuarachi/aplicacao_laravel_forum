ff89b4d7f5df206ad3ad1a3c647c3ba73fd61208
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2020-02-08 15:52:08 -0300

    script-full-scan

--- app/Samuel/Script.php
+++ app/Samuel/Script.php
@@ -15,9 +15,9 @@ class Script {
 
         $this->clearTeste1Folder();
         $slugify = new \Cocur\Slugify\Slugify();
-        $stateSlug = 'bahia';
-        $citySlug = 'salvador';
-        $url = 'https://www.travesticomlocal.com.br/salvador/';
+        $stateSlug = 'ceara';
+        $citySlug = 'fortaleza';
+        $url = 'https://www.travesticomlocal.com.br/fortaleza/';
 
         $stateFind = $stateModel->where('slug', $stateSlug)->first();
         $cityFind = $cityModel->where('slug', $citySlug)->where('state_id', $stateFind->id)->first();
