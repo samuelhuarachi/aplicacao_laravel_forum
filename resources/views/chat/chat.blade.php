7dda404d8c52293b6cdec2ba67c4a3c5a157c9b4
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2020-05-11 13:44:28 -0300

    bosta dfasfdsadfasf

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -7,6 +7,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
+        
             @include('_message-success')
             @include('_message-error2')
             @if ($tokenClient && isset($reponseAuthClient) && $reponseAuthClient->email_verified == false)
